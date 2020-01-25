8eb146d55b4586bc4e4ea0732e3736399487494c
Kathlia Carlos <lia.carlos23@gmail.com>
2020-01-25 17:55:39 +0800

    add feature to script.js

--- rockPaperScissors/script.js
+++ rockPaperScissors/script.js
@@ -1,4 +1,17 @@
 
-
+function computerPlay(){
+    let random = parseInt((Math.random() * 3) + 1);
+    switch (random) {
+        case 1 :
+            return("Rock")
+        break;
+        case 2 :
+            return("Paper")
+        break;
+        case 3 :
+            return("Scissors")
+        break;
+    }
+}
 
 
