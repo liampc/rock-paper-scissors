f7ab82bd98aa650a568ee1e48bc1081bf928ee46
Kathlia Carlos <lia.carlos23@gmail.com>
2020-01-29 10:33:52 +0800

    add first working game

--- rockPaperScissors/script.js
+++ rockPaperScissors/script.js
@@ -1,51 +1,69 @@
 
+
 function computerPlay(){
     let random = parseInt((Math.random() * 3) + 1);
     switch (random) {
         case 1 :
-            return("Rock")
+            return("rock")
         break;
         case 2 :
-            return("Paper")
+            return("paper")
         break;
         case 3 :
-            return("Scissors")
+            return("scissors")
         break;
     }
 }
 
+function playerPlay() {
+    let player = prompt("Choose Rock, Paper or Scissors");
+    return player.toLowerCase();
+}
 
 
 function playRound(playerSelection, computerSelection){
-    let player = playerSelection.toLowerCase();
-    let comp = computerSelection.toLowerCase();
+    computerSelection = computerPlay();
+    playerSelection = playerPlay();
+    let player = playerSelection;
+    let comp = computerSelection;
     let r = "rock";
     let p = "paper";
     let s = "scissors";
-
-    if 
-       ((player == r && comp == p) ||
-       (player == p && comp == s) ||
-       (player == s && comp == r)) {
-         return("You Lose! " + comp + " beats " + player);
-       }  
-    else if 
-       ((player == r && comp == s) ||
-       (player == p && comp == r) ||
-       (player == s && comp == p )) 
-       {
-         return("You Win! " + player + " beats " + comp);
-       }  
+    
+    if (
+        (player == r && comp == s) ||
+        (player == p && comp == r) ||
+        (player == s && comp == p)
+        ) { 
+           
+            return ("You win! " + player + " beats " + comp);
+            }
+    else if (
+        (player == r && comp == p) ||
+        (player == p && comp == s) ||
+        (player == s && comp == r)
+        ) {
+           
+            return ("You lose! " + comp + " beats " + player);
+        }
     else if (player == comp) {
-        return ("Samesies! Try Again");
+    
+        return ("It's a tie!")
     }
     else {
-        return ("Rock, Paper, Scissors only!");
+        
+        return ("Rock, Paper, Scissors only!")
     }
+    
 }
 
 
-const playerSelection = prompt("Rock, Paper or Scissors?");
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+    for (let i = 0; i < 5; i++){
+        console.log(playRound());
+    }
+    alert("Game over")
+}
+
+console.log(game());
 
