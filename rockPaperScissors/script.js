486325ffbce66e8bedb9992c09c261ced18037bf
Kathlia Carlos <lia.carlos23@gmail.com>
2020-02-15 11:43:00 +0800

    change computerPlay and playerPlay function

--- rockPaperScissors/script.js
+++ rockPaperScissors/script.js
@@ -1,28 +1,21 @@
-
 function computerPlay(){
-    let random = (Math.floor(Math.random() * 4));
-    switch (random) {
-        case 1 :
-            return("rock")
-        break;
-        case 2 :
-            return("paper")
-        break;
-        case 3 :
-            return("scissors")
-        break;
-    }
+    let random = (Math.floor(Math.random() * 3));
+    let choices = ["rock", "paper", "scissors"];
+    let computer = choices[random];
+    return computer;
 }
 
 function playerPlay() {
-    let player = document.querySelectorAll("#option>.choices");
-    console.log (player.toLowerCase());
-}
+    choose = prompt("Choose your weapon");
+    playerChoice = toLowerCase(choose);
+    return playerChoice;
+    }
+  
 
 
 function playRound(playerSelection, computerSelection){
-    computerSelection = computerPlay();
-    playerSelection = playerPlay();
+    playerSelection = playerPlay;
+    computerSelection = computerPlay;
     let player = playerSelection;
     let comp = computerSelection;
     let r = "rock";
@@ -32,18 +25,18 @@ function playRound(playerSelection, computerSelection){
     if ((player == r && comp == s) ||
         (player == p && comp == r) ||
         (player == s && comp == p)) { 
-        alert("You win! " + player + " beats " + comp);
+            console.log ("You win! " + player + " beats " + comp);
     }
     else if ((player == r && comp == p) ||
             (player == p && comp == s) ||
             (player == s && comp == r)) {
-        alert("You lose! " + comp + " beats " + player);
+            console.log ("You lose! " + comp + " beats " + player);
     }
     else if (player == comp) {
-        alert("It's a tie!");
+            console.log ("It's a tie!");
     }
     else {
-        alert("Rock, Paper, Scissors only!");
+            console.log ("Rock, Paper, Scissors only!");
     }  
 }
 
@@ -51,6 +44,8 @@ function playRound(playerSelection, computerSelection){
 function game(n) {
      n = parseInt(document.getElementById("rounds").value);
      for (let i = 0; i < n; i++){
+        console.log(computerPlay())
+        console.log(playerPlay())
         console.log(playRound());
     }
 }
@@ -60,5 +55,4 @@ function game(n) {
 
 let playBtn = document.getElementById("play");
 playBtn.addEventListener('click', game)
-
 
